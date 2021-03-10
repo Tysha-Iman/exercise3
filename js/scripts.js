5a10055c9a88970c674b2226a37b9ae2fde33dc1
Latysha Mohamed <Tysha@Latyshas-MacBook-Pro.local>
2021-03-10 03:57:40 -0700

    getting the hang of this

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 
-function Quote (quoteText, author, tags, color) {
-	this.quote = quoteText;
-	this.author = author;
+function Artwork (artwork, artist, song, tags, color) {
+	this.artwork = artwork;
+	this.artist = artist;
+	this.song = song;
 	this.tags = tags; 
 	this.color = color;
 	this.display = function() {
@@ -11,34 +12,35 @@ function Quote (quoteText, author, tags, color) {
 			container.addClass(tag);
 		})
 		container.css("background", this.color)
-		container.addClass("quote")
+		container.addClass("artwork")
 
-		var quoteString = "";
-		quoteString += "<p>" + this.quote + "</p>";
-		quoteString += "<cite>" + this.author + "</cite";
+		var artworkString = "";
+		artworkString += "<img src=>" + this.artwork + ;
+		artworkString += "<cite>" + this.artist + "</cite";
+		artworkString += "<cite>" + this.song + "</cite";
 
-		container.html(quoteString)
-		$(".quotes").prepend(container)
+		container.html(artworkString)
+		$(".artworks").prepend(container)
 	}
 }
 
-var quotes = [
-	new Quote('"we don\'t make mistakes, just happy little accidents."', "Bob Ross", ["painting", "mistakes"], "green"),
-	new Quote('"Creativity takes courage"', "Henry Matisse", ["painting", "creativity"], "light blue"),
+var artworks = [
+	new Artwork("img/Bravado.JPG", "Lorde", "Bravado", ["Lorde"], ""),
+	new Artwork("img/Bury_a_Friend.png", "" , [""], "")
 ]
 
 //global taglist
 var tagList = []
 
-quotes.forEach(function(quote){
-	quote.display();
-	quote.tags.forEach(function(tag){
+artworks.forEach(function(artwork){
+	artwork.display();
+	artwork.tags.forEach(function(tag){
 		//check to see if tag has been added to taglist
-		if (!tagList.includes(tag)) {
+		if(!tagList.includes(tag)) {
 			//if it isn't added, add it
 			tagList.push(tag);
 			//and also make a button for it.
-			$(".buttons").prepend("<button class='filter' id'" + tag + "'>" + tag + "</buttons>")
+			$(".buttons").prepend("<button class='filter' id='" + tag + "'>" + tag + "</buttons>")
 			}
 		})
 	})
@@ -48,7 +50,7 @@ $(".filter").on("click", function() {
 	var tag = $(this).attr("id");
 	console.log(tag)
 
-	$(".quote").not("." + tag).hide();
+	$(".artwork").not("." + tag).hide();
 	$("." + tag).fadeIn();
 
 	$(".filter").removeClass("active")
